8c0f24bab03470880366de340a16074749f63cb7
Brock T <brockthigpen@yahoo.com>
2020-01-24 11:01:41 -0500

    corrected sql syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burgers {
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR
+    (255) NOT NULL,
     eaten BOOLEAN DEFAULT false,
-};
+);
