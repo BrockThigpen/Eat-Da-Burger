b498cd7db46ed2b27571d6c59311ce8cd62a4d17
Brock Thigpen <brockthigpen@yahoo.com>
2020-10-17 13:39:52 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "BThigpen*8149",
+    password: "yourPasswordHere",
     database: "burger_db"
   });
 };
